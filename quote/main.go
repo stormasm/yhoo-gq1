2ce296225b60787ad1d4835517c3ae8629aa69b6
Michael Angerman <stormasm@gmail.com>
2019-05-16 21:16:40 -0700

    cr

--- quote/main.go
+++ quote/main.go
@@ -1,12 +1,6 @@
 /*
-Package quote is free quote downloader library and cli
-
-Downloads daily/weekly/monthly/yearly historical price quotes from Yahoo
-and daily/intraday data from Google
-
 Copyright 2018 Mark Chenoweth
 Licensed under terms of MIT license
-
 */
 
 package main
@@ -20,41 +14,6 @@ import (
 
 	"github.com/stormasm/yhoo-gq1"
 )
-
-var usage = `Usage:
-  quote -h | -help
-  quote -v | -version
-  quote <market> [-output=<outputFile>]
-  quote [-years=<years>|(-start=<datestr> [-end=<datestr>])] [options] [-infile=<filename>|<symbol> ...]
-
-Options:
-  -h -help             show help
-  -v -version          show version
-  -years=<years>       number of years to download [default=5]
-  -start=<datestr>     yyyy[-[mm-[dd]]]
-  -end=<datestr>       yyyy[-[mm-[dd]]] [default=today]
-  -infile=<filename>   list of symbols to download
-  -outfile=<filename>  output filename
-  -period=<period>     1m|3m|5m|15m|30m|1h|2h|4h|6h|8h|12h|d|3d|w|m [default=d]
-  -source=<source>     yahoo|google [default=yahoo]
-  -token=<tiingo_tok>  tingo api token [default=TIINGO_API_TOKEN]
-  -format=<format>     (csv|json|hs|ami) [default=csv]
-  -adjust=<bool>       adjust yahoo prices [default=true]
-  -all=<bool>          all in one file (true|false) [default=false]
-  -log=<dest>          filename|stdout|stderr|discard [default=stdout]
-  -delay=<ms>          delay in milliseconds between quote requests
-
-Note: not all periods work with all sources
-
-Valid markets:
-etfs:       etf
-exchanges:  nasdaq,nyse,amex
-market cap: megacap,largecap,midcap,smallcap,microcap,nanocap
-sectors:    basicindustries,capitalgoods,consumerdurables,consumernondurable,
-            consumerservices,energy,finance,healthcare,miscellaneous,
-            utilities,technolog,transportation
-all:        allmarkets
-`
 
 const (
 	version    = "0.2"
@@ -81,9 +40,7 @@ type quoteflags struct {
 func check(e error) {
 	if e != nil {
 		fmt.Printf("\nerror: %v\n\n", e)
-		fmt.Println(usage)
 		os.Exit(0)
-		//panic(e)
 	}
 }
 
